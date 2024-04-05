b8278eca80feed4f509deac2d85e2ea71a570b62
Yash Srivastava <31058288+Yash9988@users.noreply.github.com>
2024-04-05 15:29:59 +1100

    Add files via upload
    Updated capture_thermal_image.py to solely acquire images from a FLIR camera

--- SensorCommunication/capture_thermal_image.py
+++ SensorCommunication/capture_thermal_image.py
@@ -17,6 +17,7 @@ import os
 from datetime import datetime
 
 timestamp = datetime.now().strftime('%Y%m%d-%H%M%S') #time stamp variable for storing an image in a folder.
+isFlirCam = False
 
 class StreamMode:
     """
@@ -94,193 +95,198 @@ def acquire_images(cam, nodemap, nodemap_tldevice):
     """
 
     print('*** IMAGE ACQUISITION ***\n')
-    try:
-        result = True
+    result = True
 
-        # Set acquisition mode to continuous
-        #
-        #  *** NOTES ***
-        #  Because the example acquires and saves n images, setting acquisition
-        #  mode to continuous lets the example finish. If set to single frame
-        #  or multiframe (at a lower number of images), the example would just
-        #  hang. This would happen because the example has been written to
-        #  acquire n images while the camera would have been programmed to
-        #  retrieve less than that.
-        #
-        #  Setting the value of an enumeration node is slightly more complicated
-        #  than other node types. Two nodes must be retrieved: first, the
-        #  enumeration node is retrieved from the nodemap; and second, the entry
-        #  node is retrieved from the enumeration node. The integer value of the
-        #  entry node is then set as the new value of the enumeration node.
-        #
-        #  Notice that both the enumeration and the entry nodes are checked for
-        #  availability and readability/writability. Enumeration nodes are
-        #  generally readable and writable whereas their entry nodes are only
-        #  ever readable.
-        #
-        #  Retrieve enumeration node from nodemap
+    if(isFlirCam):
+        try:
+            # Set acquisition mode to continuous
+            #
+            #  *** NOTES ***
+            #  Because the example acquires and saves n images, setting acquisition
+            #  mode to continuous lets the example finish. If set to single frame
+            #  or multiframe (at a lower number of images), the example would just
+            #  hang. This would happen because the example has been written to
+            #  acquire n images while the camera would have been programmed to
+            #  retrieve less than that.
+            #
+            #  Setting the value of an enumeration node is slightly more complicated
+            #  than other node types. Two nodes must be retrieved: first, the
+            #  enumeration node is retrieved from the nodemap; and second, the entry
+            #  node is retrieved from the enumeration node. The integer value of the
+            #  entry node is then set as the new value of the enumeration node.
+            #
+            #  Notice that both the enumeration and the entry nodes are checked for
+            #  availability and readability/writability. Enumeration nodes are
+            #  generally readable and writable whereas their entry nodes are only
+            #  ever readable.
+            #
+            #  Retrieve enumeration node from nodemap
 
-        # In order to access the node entries, they have to be casted to a pointer type (CEnumerationPtr here)
-        node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
-        if not PySpin.IsReadable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
-            print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
-            return False
-
-        # Retrieve entry node from enumeration node
-        node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
-        if not PySpin.IsReadable(node_acquisition_mode_continuous):
-            print('Unable to set acquisition mode to continuous (entry retrieval). Aborting...')
-            return False
-
-        # Retrieve integer value from entry node
-        acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
-
-        # Set integer value from entry node as new value of enumeration node
-        node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
-
-        print('Acquisition mode set to continuous...')
-
-        #  Begin acquiring images
-        #
-        #  *** NOTES ***
-        #  What happens when the camera begins acquiring images depends on the
-        #  acquisition mode. Single frame captures only a single image, multi
-        #  frame catures a set number of images, and continuous captures a
-        #  continuous stream of images. Because the example calls for the
-        #  retrieval of n images, continuous mode has been set.
-        #
-        #  *** LATER ***
-        #  Image acquisition must be ended when no more images are needed.
-        cam.BeginAcquisition()
-
-        print('Acquiring images...')
-
-        #  Retrieve device serial number for filename
-        #
-        #  *** NOTES ***
-        #  The device serial number is retrieved in order to keep cameras from
-        #  overwriting one another. Grabbing image IDs could also accomplish
-        #  this.
-        device_serial_number = ''
-        node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
-        if PySpin.IsReadable(node_device_serial_number):
-            device_serial_number = node_device_serial_number.GetValue()
-            print('Device serial number retrieved as %s...' % device_serial_number)
-
-        # Retrieve, convert, and save images
-
-        # Create ImageProcessor instance for post processing images
-        processor = PySpin.ImageProcessor()
-
-        # Set default image processor color processing method
-        #
-        # *** NOTES ***
-        # By default, if no specific color processing algorithm is set, the image
-        # processor will default to NEAREST_NEIGHBOR method.
-        processor.SetColorProcessing(PySpin.SPINNAKER_COLOR_PROCESSING_ALGORITHM_HQ_LINEAR)
-
-        for i in range(NUM_IMAGES):
-            try:
-
-                #  Retrieve next received image
-                #
-                #  *** NOTES ***
-                #  Capturing an image houses images on the camera buffer. Trying
-                #  to capture an image that does not exist will hang the camera.
-                #
-                #  *** LATER ***
-                #  Once an image from the buffer is saved and/or no longer
-                #  needed, the image must be released in order to keep the
-                #  buffer from filling up.
-                image_result = cam.GetNextImage(1000)
-
-                #  Ensure image completion
-                #
-                #  *** NOTES ***
-                #  Images can easily be checked for completion. This should be
-                #  done whenever a complete image is expected or required.
-                #  Further, check image status for a little more insight into
-                #  why an image is incomplete.
-                if image_result.IsIncomplete():
-                    print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
-
-                else:
-
-                    #  Print image information; height and width recorded in pixels
-                    #
-                    #  *** NOTES ***
-                    #  Images have quite a bit of available metadata including
-                    #  things such as CRC, image status, and offset values, to
-                    #  name a few.
-                    width = image_result.GetWidth()
-                    height = image_result.GetHeight()
-                    print('Grabbed Image %d, width = %d, height = %d' % (i, width, height))
-
-                    #  Convert image to mono 8
-                    #
-                    #  *** NOTES ***
-                    #  Images can be converted between pixel formats by using
-                    #  the appropriate enumeration value. Unlike the original
-                    #  image, the converted one does not need to be released as
-                    #  it does not affect the camera buffer.
-                    #
-                    #  When converting images, color processing algorithm is an
-                    #  optional parameter.
-                    image_converted = processor.Convert(image_result, PySpin.PixelFormat_Mono8)
-
-                    # Create a unique filename
-                    if device_serial_number:
-                        filename = 'Acquisition-%s-%d.jpg' % (device_serial_number, i)
-                    else:  # if serial number is empty
-                        filename = 'Acquisition-%d.jpg' % i
-
-                    # Create the base directory 'Acquisition' if it does not exist
-                    base_dir = 'Acquisition'
-                    if not os.path.exists(base_dir):
-                        os.makedirs(base_dir)
-
-
-                    # Create a timestamped subdirectory
-                    
-                    timestamped_subdir = os.path.join(base_dir, timestamp)
-                    if not os.path.exists(timestamped_subdir):
-                        os.makedirs(timestamped_subdir)
-
-                    # Construct the full file path
-                    full_file_path = os.path.join(timestamped_subdir, filename)
-
-                    #  Save image
-                    #
-                    #  *** NOTES ***
-                    #  The standard practice of the examples is to use device
-                    #  serial numbers to keep images of one device from
-                    #  overwriting those of another.
-                    image_converted.Save(full_file_path)
-                    print('Image saved at %s' % full_file_path)
-
-                    #  Release image
-                    #
-                    #  *** NOTES ***
-                    #  Images retrieved directly from the camera (i.e. non-converted
-                    #  images) need to be released in order to keep from filling the
-                    #  buffer.
-                    image_result.Release()
-                    print('')
-
-            except PySpin.SpinnakerException as ex:
-                print('Error: %s' % ex)
+            # In order to access the node entries, they have to be casted to a pointer type (CEnumerationPtr here)
+            node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+            if not PySpin.IsReadable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
+                print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
                 return False
 
-        #  End acquisition
-        #
-        #  *** NOTES ***
-        #  Ending acquisition appropriately helps ensure that devices clean up
-        #  properly and do not need to be power-cycled to maintain integrity.
-        cam.EndAcquisition()
+            # Retrieve entry node from enumeration node
+            node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
+            if not PySpin.IsReadable(node_acquisition_mode_continuous):
+                print('Unable to set acquisition mode to continuous (entry retrieval). Aborting...')
+                return False
 
-    except PySpin.SpinnakerException as ex:
-        print('Error: %s' % ex)
-        return False
+            # Retrieve integer value from entry node
+            acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+
+            # Set integer value from entry node as new value of enumeration node
+            node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+
+            print('Acquisition mode set to continuous...')
+
+            #  Begin acquiring images
+            #
+            #  *** NOTES ***
+            #  What happens when the camera begins acquiring images depends on the
+            #  acquisition mode. Single frame captures only a single image, multi
+            #  frame catures a set number of images, and continuous captures a
+            #  continuous stream of images. Because the example calls for the
+            #  retrieval of n images, continuous mode has been set.
+            #
+            #  *** LATER ***
+            #  Image acquisition must be ended when no more images are needed.
+            cam.BeginAcquisition()
+
+            print('Acquiring images...')
+
+            #  Retrieve device serial number for filename
+            #
+            #  *** NOTES ***
+            #  The device serial number is retrieved in order to keep cameras from
+            #  overwriting one another. Grabbing image IDs could also accomplish
+            #  this.
+            device_serial_number = ''
+            node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+            if PySpin.IsReadable(node_device_serial_number):
+                device_serial_number = node_device_serial_number.GetValue()
+                print('Device serial number retrieved as %s...' % device_serial_number)
+
+            # Retrieve, convert, and save images
+
+            # Create ImageProcessor instance for post processing images
+            processor = PySpin.ImageProcessor()
+
+            # Set default image processor color processing method
+            #
+            # *** NOTES ***
+            # By default, if no specific color processing algorithm is set, the image
+            # processor will default to NEAREST_NEIGHBOR method.
+            processor.SetColorProcessing(PySpin.SPINNAKER_COLOR_PROCESSING_ALGORITHM_HQ_LINEAR)
+
+            for i in range(NUM_IMAGES):
+                try:
+
+                    #  Retrieve next received image
+                    #
+                    #  *** NOTES ***
+                    #  Capturing an image houses images on the camera buffer. Trying
+                    #  to capture an image that does not exist will hang the camera.
+                    #
+                    #  *** LATER ***
+                    #  Once an image from the buffer is saved and/or no longer
+                    #  needed, the image must be released in order to keep the
+                    #  buffer from filling up.
+                    image_result = cam.GetNextImage(1000)
+
+                    #  Ensure image completion
+                    #
+                    #  *** NOTES ***
+                    #  Images can easily be checked for completion. This should be
+                    #  done whenever a complete image is expected or required.
+                    #  Further, check image status for a little more insight into
+                    #  why an image is incomplete.
+                    if image_result.IsIncomplete():
+                        print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+
+                    else:
+
+                        #  Print image information; height and width recorded in pixels
+                        #
+                        #  *** NOTES ***
+                        #  Images have quite a bit of available metadata including
+                        #  things such as CRC, image status, and offset values, to
+                        #  name a few.
+                        width = image_result.GetWidth()
+                        height = image_result.GetHeight()
+                        print('Grabbed Image %d, width = %d, height = %d' % (i, width, height))
+
+                        #  Convert image to mono 8
+                        #
+                        #  *** NOTES ***
+                        #  Images can be converted between pixel formats by using
+                        #  the appropriate enumeration value. Unlike the original
+                        #  image, the converted one does not need to be released as
+                        #  it does not affect the camera buffer.
+                        #
+                        #  When converting images, color processing algorithm is an
+                        #  optional parameter.
+                        image_converted = processor.Convert(image_result, PySpin.PixelFormat_Mono8)
+
+                        # Create a unique filename
+                        if device_serial_number:
+                            filename = 'Acquisition-%s-%d.jpg' % (device_serial_number, i)
+                        else:  # if serial number is empty
+                            filename = 'Acquisition-%d.jpg' % i
+
+                        # Create the base directory 'Acquisition' if it does not exist
+                        base_dir = 'Acquisition'
+                        if not os.path.exists(base_dir):
+                            os.makedirs(base_dir)
+
+
+                        # Create a timestamped subdirectory
+                        
+                        timestamped_subdir = os.path.join(base_dir, timestamp)
+                        if not os.path.exists(timestamped_subdir):
+                            os.makedirs(timestamped_subdir)
+
+                        # Construct the full file path
+                        full_file_path = os.path.join(timestamped_subdir, filename)
+
+                        #  Save image
+                        #
+                        #  *** NOTES ***
+                        #  The standard practice of the examples is to use device
+                        #  serial numbers to keep images of one device from
+                        #  overwriting those of another.
+                        image_converted.Save(full_file_path)
+                        print('Image saved at %s' % full_file_path)
+
+                        #  Release image
+                        #
+                        #  *** NOTES ***
+                        #  Images retrieved directly from the camera (i.e. non-converted
+                        #  images) need to be released in order to keep from filling the
+                        #  buffer.
+                        image_result.Release()
+                        print('')
+
+                except PySpin.SpinnakerException as ex:
+                    print('Error: %s' % ex)
+                    return False
+
+            #  End acquisition
+            #
+            #  *** NOTES ***
+            #  Ending acquisition appropriately helps ensure that devices clean up
+            #  properly and do not need to be power-cycled to maintain integrity.
+            cam.EndAcquisition()
+
+        except PySpin.SpinnakerException as ex:
+            print('Error: %s' % ex)
+            return False
+
+    else:
+        result = False 
+        print("Acquisition Cancelled. Not a FLIR camera.")
 
     return result
 
@@ -297,10 +303,14 @@ def print_device_info(nodemap):
     :rtype: bool
     """
 
+    global isFlirCam
+
     print('*** DEVICE INFORMATION ***\n')
 
     try:
         result = True
+        isFlirCam = False
+        
         node_device_information = PySpin.CCategoryPtr(nodemap.GetNode('DeviceInformation'))
 
         if PySpin.IsReadable(node_device_information):
@@ -309,6 +319,11 @@ def print_device_info(nodemap):
                 node_feature = PySpin.CValuePtr(feature)
                 print('%s: %s' % (node_feature.GetName(),
                                   node_feature.ToString() if PySpin.IsReadable(node_feature) else 'Node not readable'))
+
+                if node_feature.GetName() == "DeviceVendorName" and "FLIR" in node_feature.ToString():
+                    isFlirCam = True
+
+            print(isFlirCam)
 
         else:
             print('Device control information not readable.')
